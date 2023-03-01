1ca0473ac87f0d6d3dda4d6b8a2e1c6df1073078
swetha-balakrishnan <126647121+swetha-balakrishnan@users.noreply.github.com>
2023-03-01 12:03:11 +0530

    shop

---
+++ count_the.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	string s;
+	int i,j=0,n,co=0;
+	string str[50];
+	getline(cin,s);
+	n=s.length();
+	for(i=0;i<n;i++)
+	{
+		if(s[i]!=' ')
+		{
+		  str[j]+=s[i];
+		  	
+		}
+		if(s[i]== ' ')
+		{
+			j++;
+		}
+	}
+	for(i=0;i<=j;i++)
+	{
+		if(str[i]=="the")
+		{
+			co++;
+		}
+	}
+	cout<<co;
+	
+}
